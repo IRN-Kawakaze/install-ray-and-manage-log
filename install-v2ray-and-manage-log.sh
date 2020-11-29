452fb98a56c3cfb407a35e36206c21cf68b3270d
IRN-Kawakaze <41798687+IRN-Kawakaze@users.noreply.github.com>
2020-11-29 17:31:12 +0800

    Upload install-v2ray-and-manage-log.sh

---
+++ install-v2ray-and-manage-log.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+# Copyright(C) by Kawakaze 2020.
+# v0.1.0
+
+# 环境变量，路径尾部均不包含/
+
+# v2ray配置文件存放路径
+v2rayconfigpath=/usr/local/etc/v2ray
+# v2ray日志文件存放路径
+v2raylogpath=/tmp/log/v2ray
+# v2ray日志文件存放路径的上级目录
+v2raylogpathparentdirectory=/tmp/log
+# v2ray服务文件的绝对路径
+v2rayservice=/etc/systemd/system/v2ray.service
+# 脚本存放路径
+scriptspath=/etc/userscripts
+# 用户crontab文件的绝对路径
+usercrontabfile=/var/spool/cron/root
+
+
+# 检查依赖
+command -v wget > /dev/null 2>&1 || { echo "ERROR\: You should install \"wget\" first.";exit 1; }
+command -v curl > /dev/null 2>&1 || { echo "ERROR\: You should install \"curl\" first.";exit 1; }
+command -v crontab > /dev/null 2>&1 || { echo "ERROR\: You should install \"crontab\" first.";exit 1; }
+command -v systemctl > /dev/null 2>&1 || { echo "ERROR\: You should install \"systemctl\" first.";exit 1; }
+command -v rm > /dev/null 2>&1 || { echo "ERROR\: Cannot run \"rm\" command.";exit 1; }
+command -v mkdir > /dev/null 2>&1 || { echo "ERROR\: Cannot run \"mkdir\" command.";exit 1; }
+command -v chmod > /dev/null 2>&1 || { echo "ERROR\: Cannot run \"chmod\" command.";exit 1; }
+command -v cat > /dev/null 2>&1 || { echo "ERROR\: Cannot run \"cat\" command.";exit 1; }
+command -v sed > /dev/null 2>&1 || { echo "ERROR\: Cannot run \"sed\" command.";exit 1; }
+
+# 检查和GitHub的网络连接是否正常，如果不正常则直接退出
+wget -4 --spider --quiet --tries=3 --timeout=3 https://raw.githubusercontent.com || { echo "ERROR\: Cannot connect to GitHub.";exit 1; }    
+
+# 安装v2ray，如果安装失败则直接退出
+bash <(curl https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh) || { echo "ERROR\: Install v2ray failed.";exit 1; }    
+
+# 删除可能存在的多配置文件
+rm -f $v2rayconfigpath/*
+
+# 创建日志文件存放路径
+mkdir -p $v2raylogpath/
+
+# 创建日志文件
+echo "" > $v2raylogpath/access.log
+echo "" > $v2raylogpath/error.log
+
+# 给日志文件及其存放路径赋权
+chmod 1777 $v2raylogpathparentdirectory
+chmod 1777 $v2raylogpath
+chmod 1666 $v2raylogpath/*
+
+# 创建日志管理脚本存放路径
+mkdir -p $scriptspath/
+
+# 创建日志管理脚本
+cat <<EOF > $scriptspath/cleanv2raylog.sh
+#!/bin/sh
+
+# 删除现有的日志备份文件
+rm -f $v2raylogpath/access.log.backup
+
+# 生成当前日志的备份
+cp $v2raylogpath/access.log $v2raylogpath/access.log.backup
+
+# 清空当前的日志文件
+echo "" > $v2raylogpath/access.log
+
+EOF
+
+# 给日志管理脚本赋权
+chmod +x $scriptspath/cleanv2raylog.sh
+
+# 添加自动清理日志的定时任务，每个月清理一次
+echo "0 5 1 * * $scriptspath/cleanv2raylog.sh" >> $usercrontabfile
+
+# 修改v2ray的最大句柄数和最大进程数
+sed -i '/\[Service\]/a\LimitCORE=infinity\nLimitNOFILE=16384\nLimitNPROC=16384\n' $v2rayservice
+systemctl daemon-reload
+
+# 打印v2ray配置文件路径，以便修改
+echo "Config file path:"
+echo "$v2rayconfigpath/config.json"
+
